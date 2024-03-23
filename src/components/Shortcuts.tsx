89c9f637583cda74c7064ca6b7191537fbf8ba30
tabcat <tabcat00@proton.me>
2024-03-23 07:30:19 -0500

    working shortcuts

--- src/components/Shortcuts.tsx
+++ src/components/Shortcuts.tsx
@@ -1,11 +1,30 @@
-import { DragEvent } from 'react';
+import { DragEvent, useState, type SyntheticEvent } from 'react';
 
-// const Shortcut = (url: string) => {
+const Shortcut = ({ url, key }: { url: string, key: number }) => {
+  // Assuming the favicon is located at the root as /favicon.ico
+  const parsedUrl = new URL(url)
+  const faviconUrl = new URL('/favicon.ico', url).href
+  const siteName = parsedUrl.hostname.split('.').slice(-2)[0]
 
-//   return <></>
-// }
+  const handleError = (e: SyntheticEvent<HTMLImageElement>) => {
+    e.currentTarget.src = './favicon.ico'
+  }
+
+  return (
+    <div className="shortcut" key={key}>
+      <a href={url} rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
+        <div style={{ alignItems: 'center' }}>
+          <img src={faviconUrl} alt={url} draggable={false} style={{ width: '64px', height: '64px', display: "block" }} onError={handleError}/>
+          <p className="sheen-text" style={{ fontSize: "12px", marginTop: "5px", marginBottom: "5px", textAlign: "center" }}>{siteName}</p>
+        </div>
+      </a>
+    </div>
+  );
+}
 
 const Shortcuts = () => {
+  const [urls, setUrls] = useState<string[]>([])
+
   const handleDragOver = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault(); // Necessary to allow for dropping
     e.stopPropagation();
@@ -16,8 +35,19 @@ const Shortcuts = () => {
     e.stopPropagation();
     const url = e.dataTransfer.getData('text/uri-list');
     // Handle the dropped URL as needed
-    console.log(url); // For demonstration
+
+    try {
+      new URL(url)
+
+      if (urls.includes(url)) {
+        setUrls([...urls.filter((u: string) => url !== u)])
+      } else {
+        setUrls([...urls, url])
+      }
+    } catch (e) {}
   };
+
+  const existingUrls = urls.length > 0
 
   return (
     <div
@@ -25,9 +55,15 @@ const Shortcuts = () => {
       className="unselectable"
       onDragOver={handleDragOver}
       onDrop={handleDrop}
+      style={{
+        justifyContent: existingUrls ? "left" : "center"
+      }}
     >
-      <p className="sheen-text">Drop Links Here</p>
-      {/* <Shortcut /> */}
+      {
+        existingUrls
+          ? urls.map((url: string, key: number)=> Shortcut({ url, key }))
+          : <p className="sheen-text">Drop Links Here</p>
+      }
     </div>
   );
 };
